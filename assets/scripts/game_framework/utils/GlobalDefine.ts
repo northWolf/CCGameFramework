d012e7eb581adec7801fb1e2d24b6ed57f095183
lz3263540140 <3263540140@qq.com>
2019-12-21 18:49:07 +0800

    添加混入的功能。

--- assets/scripts/game_framework/utils/GlobalDefine.ts
+++ assets/scripts/game_framework/utils/GlobalDefine.ts
@@ -1,3 +1,14 @@
-export function isNull(obj:any){
+export function isNull(obj: any) {
     return obj == undefined || obj == null;
+}
+
+/**
+ * Mixin，组合可重用组件来创建类
+ */
+export function applyMixins(derivedCtor: any, baseCtors: any[]) {
+    baseCtors.forEach(baseCtor => {
+        Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+            derivedCtor.prototype[name] = baseCtor.prototype[name];
+        })
+    });
 }
