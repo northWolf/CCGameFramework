22d0f554e570bf23175145151a67dccd5631dbaf
414255309@qq.com <3263540140@qq.com>
2020-04-07 01:44:43 +0800

    LoginView增加 热更新和清理LocalStorage的演示

--- assets/scripts/game_main/module/login/LoginView.ts
+++ assets/scripts/game_main/module/login/LoginView.ts
@@ -11,6 +11,7 @@ import ChannelID from "../../../game_framework/sdk/ChannelID";
 import DefaultChannel from "../../sdk/default/DefaultChannel";
 import LoginConst from "./LoginConst";
 import ResourceItem from "../../../game_framework/resource/ResourceItem";
+import LocalStorageUtils from "../../../game_framework/utils/LocalStorageUtils";
 
 export default class LoginView extends BaseFguiView {
 
@@ -35,15 +36,15 @@ export default class LoginView extends BaseFguiView {
 
     private onUILoaded(err: string, res: ResourceItem) {
         fgui.UIPackage.addPackage(App.PathUtil.getCombinePath(LoginConst.LOGIN_UI_PKG.path, LoginConst.LOGIN_UI_PKG.name));
-        this._view = fgui.UIPackage.createObject(LoginConst.LOGIN_UI_PKG.name, "Main").asCom;
+        this._view = fgui.UIPackage.createObject(LoginConst.LOGIN_UI_PKG.name, "Framework").asCom;
         this._view.makeFullScreen();
         this.addToParent();
 
-        FairyGUIUtil.GFindChild(this._view, "n1").onClick(function () {
-            Log.info("准备打开Basic");
+        FairyGUIUtil.GFindChild(this._view, "btn_log").onClick(function () {
+            Log.info("准备演示框架功能");
         }, this);
 
-        FairyGUIUtil.GFindChild(this._view, "n2").onClick(function () {
+        FairyGUIUtil.GFindChild(this._view, "btn_http_request").onClick(function () {
             let _data = new Object();
             _data["packageName"] = "com.cz.game";
             _data["channelTag"] = "unknown";
@@ -54,7 +55,7 @@ export default class LoginView extends BaseFguiView {
             });
         }, this);
 
-        FairyGUIUtil.GFindChild(this._view, "n3").onClick(function () {
+        FairyGUIUtil.GFindChild(this._view, "btn_web_socket").onClick(function () {
             Log.info("准备连接服务器");
             App.Net.createNetChannelWithIpPort(DefaultNetChannel.NetChannel_Hall.toString(),
                 NetChannelType.SOCKET, App.GlobalInfo.GateServerIp, App.GlobalInfo.GateServerPort, new ByteArrayMsgByProtobuf());
@@ -62,7 +63,7 @@ export default class LoginView extends BaseFguiView {
             App.Net.connectNetChannel(DefaultNetChannel.NetChannel_Hall.toString());
         }, this);
 
-        FairyGUIUtil.GFindChild(this._view, "n4").onClick(function () {
+        FairyGUIUtil.GFindChild(this._view, "btn_login").onClick(function () {
             Log.info("准备渠道登录");
             App.SDK.init(new DefaultChannel(ChannelID.DEFAULT));
             App.SDK.getChannel().login(null, function () {
@@ -71,28 +72,28 @@ export default class LoginView extends BaseFguiView {
 
         }, this);
 
-        FairyGUIUtil.GFindChild(this._view, "n5").onClick(function () {
+        FairyGUIUtil.GFindChild(this._view, "btn_unload_res").onClick(function () {
             Log.info("卸载MainMenu UI包资源");
             fgui.UIPackage.removePackage(LoginConst.LOGIN_UI_PKG.name);
             App.ResManager.releaseDir(LoginConst.LOGIN_UI_PKG.path);
         }, this);
 
-        FairyGUIUtil.GFindChild(this._view, "n6").onClick(function () {
+        FairyGUIUtil.GFindChild(this._view, "btn_play_bgm").onClick(function () {
             Log.info("播放背景音乐");
             App.SoundManager.playBg("sound/bgm_lobby");
         }, this);
 
-        FairyGUIUtil.GFindChild(this._view, "n7").onClick(function () {
+        FairyGUIUtil.GFindChild(this._view, "btn_stop_bgm").onClick(function () {
             Log.info("停止背景音乐");
             App.SoundManager.stopBg();
         }, this);
 
-        FairyGUIUtil.GFindChild(this._view, "n8").onClick(function () {
+        FairyGUIUtil.GFindChild(this._view, "btn_play_sound").onClick(function () {
             Log.info("播放金币音效");
             App.SoundManager.playEffect("sound/sfx_coins");
         }, this);
 
-        FairyGUIUtil.GFindChild(this._view, "n9").onClick(function () {
+        FairyGUIUtil.GFindChild(this._view, "btn_play_effect").onClick(function () {
             Log.info("插入龙骨特效到UI");
             App.ResManager.loadRes("animations/hall_act_xlch", cc.Asset, function () {
                 var asset = App.ResManager.getRes("animations/hall_act_xlch", cc.Asset);
@@ -100,6 +101,27 @@ export default class LoginView extends BaseFguiView {
                 actorNode.parent = FairyGUIUtil.GFindChild(this._view, "n10").node;
             }.bind(this));
         }, this);
+
+        FairyGUIUtil.GFindChild(this._view,"btn_hotupdate").onClick(function(){
+            Log.info("使用热更新管理器");
+            var moduleName = "inbuilt";
+            App.HotUpdateManager.init(moduleName,function(){
+                Log.info("检查资源版本完毕");
+            },function(){
+                Log.info("更新资源完毕");
+            },function(){
+                Log.info("正在更新资源，进度为：" + App.HotUpdateManager.getProgress(moduleName));
+            },function(){
+                Log.info("已是新版本");
+            });
+            App.HotUpdateManager.check(moduleName);
+        },this);
+
+        FairyGUIUtil.GFindChild(this._view,"btn_clear_local_storage").onClick(function(){
+            Log.info("清理本地缓存");
+           LocalStorageUtils.clearAll();
+        },this);
+
 
         Log.info("LoginView 创建成功");
     }
