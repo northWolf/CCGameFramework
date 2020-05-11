0e3e508fde0f3a508986ad4b9443d4420facb567
414255309@qq.com <3263540140@qq.com>
2020-05-12 00:35:06 +0800

    添加流程

--- assets/scripts/Main.ts
+++ assets/scripts/Main.ts
@@ -1,13 +1,19 @@
+import App from "./game_framework/App";
+import ProcedureLaunch from "./game_main/procedure/ProcedureLaunch";
+import ProcedureMain from "./game_main/procedure/ProcedureMain";
+import ProcedureCheckVersion from "./game_main/procedure/ProcedureCheckVersion";
+
 const {ccclass, property} = cc._decorator;
 
-import ProcedureLaunch from "./game_main/procedure/ProcedureLaunch";
-import App from "./game_framework/App";
+
 @ccclass
 export default class Main extends cc.Component {
 
     start() {
         App.Procedure.setOwner(this);
         App.Procedure.registerState(new ProcedureLaunch(this));
+        App.Procedure.registerState(new ProcedureMain(this));
+        App.Procedure.registerState(new ProcedureCheckVersion(this));
         App.Procedure.changeState(cc.js.getClassName(ProcedureLaunch));
     }
 }
